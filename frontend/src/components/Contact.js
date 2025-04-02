9089a8e3e9871d6af12c35e1af633d096cd3fc65
Dipsankar Sinha <dipsankar2002@gamil.com>
2025-04-02 15:19:23 +0530

    Bugs and Errors fix - Version 1.1

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import dipImage from "../assets/Dipsankar_pic.png";
 import nadeemImage from "../assets/Nadeem_pic.png";
+import hrithikImage from "../assets/Hrithik_pic.png";
 function Contact() {
   return (
     <div className="result-panel">
@@ -18,7 +19,11 @@ function Contact() {
             >
               GitHub
             </a>
-            <a href={"https://www.linkedin.com/in/dipsankar-sinha-77013627a"}>
+            <a
+              href={"https://www.linkedin.com/in/dipsankar-sinha-77013627a"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               LinkedIn
             </a>
           </div>
@@ -35,18 +40,32 @@ function Contact() {
             >
               GitHub
             </a>
-            <a href={"https://www.linkedin.com/in/dipsankar-sinha-77013627a"}>
+            <a
+              href={"https://www.linkedin.com/in/nadeem-khan-083072226"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               LinkedIn
             </a>
           </div>
         </div>
         <div className="contact-card">
-          <img src={"#"} alt={"Hrithik Roy"} />
+          <img src={hrithikImage} alt={"Hrithik Roy"} />
           <div>
             <h3>Hrithik Roy</h3>
             <p>KIIT University, India</p>
-            <a href={"https://github.com/the-roy123"}>GitHub</a>
-            <a href={"https://www.linkedin.com/in/dipsankar-sinha-77013627a"}>
+            <a
+              href={"https://github.com/the-roy123"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href={"https://www.linkedin.com/in/hrithik-roy-977169241/"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               LinkedIn
             </a>
           </div>
